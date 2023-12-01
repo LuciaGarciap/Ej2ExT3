23db50d6b80c85601cd40f5a2f1d242ae74e904e
LuciaGarciap <garciapulidolucia2@ceuandalucia.es>
2023-12-01 20:05:52 +0100

    Mi preferido es el de chocolate

--- ProyectoExEj2/src/Turron.java
+++ ProyectoExEj2/src/Turron.java
@@ -4,6 +4,7 @@ public class Turron {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Soy lucia y me gustan los turrones");
+		System.out.println("Pero mi preferido es el de chocolate");
 	}
 
 }
